47448e2f19fa9b2521acb5db6b2dc0f3f8daf4cb
PengzhenChan <937377257@qq.com>
2021-06-18 19:01:10 +0800

    debug

--- coursewebsite/coursewebsite/src/main/java/team/ljm/secw/controller/AttendanceController.java
+++ coursewebsite/coursewebsite/src/main/java/team/ljm/secw/controller/AttendanceController.java
@@ -1,81 +1,67 @@
 package team.ljm.secw.controller;
 
-import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import team.ljm.secw.dto.AttendanceDTO;
+import team.ljm.secw.entity.Attendance;
 import team.ljm.secw.entity.AttendanceResult;
 import team.ljm.secw.entity.Student;
-import team.ljm.secw.service.IAttendanceService;
+import team.ljm.secw.entity.StudentVo;
+import team.ljm.secw.service.AttendanceService;
 import team.ljm.secw.vo.ResponseVO;
 
+import java.util.List;
 
+@Controller
+@RequestMapping("/attendance")
 /**
- * /all?clazzId= &studentId=,学生签到界面
+ * /all?clazzId= &id=,学生签到界面
  * /insert,不要管
- * /stuUpdate?attendeAt=,插入学生签到时间,需要传一个时间过来
+ * /stuUpdate?attendedAt= ?attendanceId=? studentId=?,插入学生签到时间,需要传一个时间过来
  * /teacherAll?clazzId=,教师的签到首页
  * /stuList?attendanceId=,点击一个签到进去,获得学生的签到情况
  * /release?attendanceName=  &endAt(结束时间) &issuer=  &clazzId=,教师发布签到
  * /updateTime?id=   &endAt=   ,教师更改签到时间
  */
-
-@Controller
 public class AttendanceController {
-
     @Autowired
-    private IAttendanceService attendanceService;
-
-    @RequiresRoles("student")
-    @RequestMapping("/attendance/all")
+    private AttendanceService attendanceService;
+    @RequestMapping("/all")
     @ResponseBody
-    public ResponseVO getAll(@RequestBody Student student){
-        return new ResponseVO("200","", attendanceService.findAllAttendance(student));
+    public ResponseVO getAll(@RequestBody  Student student){
+        return new ResponseVO("200","",this.attendanceService.findAllAttendance(student));
     }
-
-    @RequiresRoles("teacher")
-    @RequestMapping("/attendance/insert")
+    @RequestMapping("/insert")
     @ResponseBody
     public ResponseVO insert(@RequestBody AttendanceResult attendanceResult){
-        return new ResponseVO("200","", attendanceService.insertStuAttendance(attendanceResult));
+        return new ResponseVO("200","",this.attendanceService.insertStuAttendance(attendanceResult));
     }
-
-    @RequiresRoles("teacher")
-    @RequestMapping("/attendance/teacherAll")
+    @RequestMapping("/teacherAll")
     @ResponseBody
-    public ResponseVO teacherAll(@RequestParam(value = "clazzId") Integer clazzId){
-        return new ResponseVO("200","", attendanceService.findTeacherAttendance(clazzId));
+    public ResponseVO teacherAll(@RequestBody Integer clazzId){
+        return new ResponseVO("200","",this.attendanceService.findTeacherAttendance(clazzId));
     }
-
-    @RequiresRoles("student")
-    @RequestMapping("/attendance/stuUpdate")
+    @RequestMapping("/stuUpdate")
     @ResponseBody
     public ResponseVO stuUpdate(@RequestBody AttendanceResult attendanceResult){
-        return new ResponseVO("200","", attendanceService.updateResult(attendanceResult));
+        return new ResponseVO("200","",this.attendanceService.updateResult(attendanceResult));
     }
-
-    @RequiresRoles("teacher")
-    @RequestMapping("/attendance/stuList")
+    @RequestMapping("/stuList")
     @ResponseBody
     public ResponseVO AttendenceList(@RequestBody Integer attendanceId){
-        return new ResponseVO("200","", attendanceService.findStuResult(attendanceId));
+        return new ResponseVO("200","",attendanceService.findStuResult(attendanceId));
     }
-
-    @RequiresRoles("teacher")
-    @RequestMapping("/attendance/release")
+    @RequestMapping("/release")
     @ResponseBody
     public ResponseVO release(@RequestBody AttendanceDTO attendance){
-        return new ResponseVO("200","", attendanceService.releaseAttendance(attendance));
+        return new ResponseVO("200","",attendanceService.releaseAttendance(attendance));
     }
-
-    @RequiresRoles("teacher")
-    @RequestMapping("/attendance/updateTime")
+    @RequestMapping("/updateTime")
     @ResponseBody
     public ResponseVO updateTime(@RequestBody AttendanceDTO attendance){
-        return new ResponseVO("200","", attendanceService.updateEndAt(attendance));
+        return new ResponseVO("200","",attendanceService.updateEndAt(attendance));
     }
 }
--- coursewebsite/coursewebsite/src/main/java/team/ljm/secw/dto/AttendanceDTO.java
+++ coursewebsite/coursewebsite/src/main/java/team/ljm/secw/dto/AttendanceDTO.java
@@ -14,6 +14,17 @@ public class AttendanceDTO {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startAt;
 
+    public Date getAttendedAt() {
+        return attendedAt;
+    }
+
+    public void setAttendedAt(Date attendedAt) {
+        this.attendedAt = attendedAt;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date attendedAt;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endAt;
